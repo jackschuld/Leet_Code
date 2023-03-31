269143ed286e0c7be95bd6a52cdc31c250a9d5bb
unknown <schuldjack@gmail.com>
2023-03-31 15:57:46 -0700

    Started isBalanced number 110.balanced-binary-tree.py but am struggling to complete successfully. Currently at 182/228 cases passed

---
+++ 110.balanced-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=110 lang=python3
+#
+# [110] Balanced Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def results(root):
+
+            if root is None:
+                return [True, 0]
+            leftheight, rightheight = results(root.left), results(root.right)
+            check = (leftheight[0] and rightheight[0] and abs(leftheight[1]-rightheight[1] <= 1))
+            print(check)
+            return [check, 1 + max(leftheight[1], rightheight[1])]
+        return results(root)[0]
+        
+        
+        
+
+# @lc code=end
+
