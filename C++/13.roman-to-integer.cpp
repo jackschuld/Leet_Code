0e85bff8628d11697d70c6019ee24e829f41f232
unknown <schuldjack@gmail.com>
2023-03-31 10:46:28 -0700

    Completed 13 roman to integer in cpp with the following message: Accepted - 3999/3999 cases passed 6ms - Your runtime beats 88.22% of cpp submissions - Your memory usage beats 99.18% of cpp submissions 5.9 MB

--- C++/13.roman-to-integer.cpp
+++ C++/13.roman-to-integer.cpp
@@ -5,10 +5,73 @@
  */
 
 // @lc code=start
+#include <iostream>
+
 class Solution {
 public:
-    int romanToInt(string s) {
-        
+    int addInt(char t) {
+        if (t == 'I') {
+            return 1;
+        }
+        else if (t == 'V') {
+            return 5;
+        }
+        else if (t == 'X') {
+            return 10;
+        }
+        else if (t == 'L') {
+            return 50;
+        }
+        else if (t == 'C') {
+            return 100;
+        }
+        else if (t == 'D') {
+            return 500;
+        }
+        else if (t == 'M') {
+            return 1000;
+        }
+        return 0;
+    }
+
+    int romanToInt(std::string s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // Check if potential subtraction case
+            if (s[i + 1]){
+                if (s[i] == 'I' && s[i + 1] == 'V') {
+                    sum += 4;
+                    i++;
+                }
+                else if (s[i] == 'I' && s[i + 1] == 'X') {
+                    sum += 9;
+                    i++;
+                }
+                else if (s[i] == 'X' && s[i + 1] == 'L') {
+                    sum += 40;
+                    i++;
+                }
+                else if (s[i] == 'X' && s[i + 1] == 'C') {
+                    sum += 90;
+                    i++;
+                }
+                else if (s[i] == 'C' && s[i + 1] == 'D') {
+                    sum += 400;
+                    i++;
+                }
+                else if (s[i] == 'C' && s[i + 1] == 'M') {
+                    sum += 900;
+                    i++;
+                }
+                else {
+                    sum += addInt(s[i]);
+                }
+            }
+            else {
+                sum += addInt(s[i]);
+            }
+        }
+        return sum;
     }
 };
 // @lc code=end
