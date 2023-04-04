fa94b9766eb9effc7fcd1f06dbaaf815886abba6
unknown <schuldjack@gmail.com>
2023-04-04 09:08:32 -0700

    Completed problem 278.first-bad-version.py with some help from the discussion board in order to implement a binary search.

---
+++ 278.first-bad-version.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=278 lang=python3
+#
+# [278] First Bad Version
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 0
+        right = n-1
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
+
+# @lc code=end
+
